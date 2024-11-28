f4070ab80df5e6fe95faf22f81d86df5c16bd781
Dillon Devera <dillondevera12@gmail.com>
2024-11-28 06:28:53 -0700

    feat: add ContentSection component to Home page for enhanced content display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import ContentSection from "@/components/ContentSection";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
    
 
       <Hero />  
+      <ContentSection />
 
     </div>
   )
